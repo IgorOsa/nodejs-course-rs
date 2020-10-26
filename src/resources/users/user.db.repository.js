7439a051856a803ec446eb880a61a0f9cde10f33
Igor Osadchyi <igor@webio.net.ua>
2020-10-26 12:44:40 +0200

    feat: improve user error handling

--- src/resources/users/user.db.repository.js
+++ src/resources/users/user.db.repository.js
@@ -1,13 +1,43 @@
 const User = require('./user.model');
+const NOT_FOUND_ERROR = require('./../../common/errors');
 
-const getAll = async () => User.find({});
+const getAll = async () => {
+  return User.find({}, (err, data) => {
+    if (err) {
+      throw new NOT_FOUND_ERROR('No users found!');
+    }
+    return data;
+  });
+};
 
-const get = async id => User.findById({ _id: id });
+const get = async id => {
+  const user = await User.findById({ _id: id }).exec();
+
+  if (!user) {
+    throw new NOT_FOUND_ERROR(`User with id ${id} was not found`);
+  }
+
+  return user;
+};
 
 const create = async user => User.create(user);
 
-const update = async user => User.updateOne({ _id: user.id }, user);
+const update = async user => {
+  const updatedUser = await User.findOneAndUpdate({ _id: user.id }, user);
 
-const remove = async id => User.deleteOne({ _id: id });
+  if (!updatedUser) {
+    throw new NOT_FOUND_ERROR(`User with id: ${user.id} not found!`);
+  }
+
+  return updatedUser;
+};
+
+const remove = async id => {
+  const deleted = await (await User.deleteOne({ _id: id })).deletedCount;
+  if (!deleted) {
+    throw new NOT_FOUND_ERROR(`User with id: ${id} not found!`);
+  }
+  return deleted;
+};
 
 module.exports = { getAll, get, create, update, remove };
