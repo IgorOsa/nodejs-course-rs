3f5e3ce7addb40f35c8f73aaebd5e89bd72d9e5d
Igor Osadchyi <igor@webio.net.ua>
2020-11-04 15:37:26 +0200

    feat: bcrypt password hashing before save to DB

--- src/common/config.js
+++ src/common/config.js
@@ -9,6 +9,7 @@ module.exports = {
   PORT: process.env.PORT,
   NODE_ENV: process.env.NODE_ENV,
   MONGO_CONNECTION_STRING: process.env.MONGO_CONNECTION_STRING,
+  DEFAULT_SALT_ROUNDS: parseInt(process.env.DEFAULT_SALT_ROUNDS, 10),
   JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
   AUTH_MODE: process.env.AUTH_MODE === 'true'
 };
