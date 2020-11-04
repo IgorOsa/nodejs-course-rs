01e35e37ffbcda480503fc85df257af4d7df21d0
Igor Osadchyi <igor@webio.net.ua>
2020-11-04 18:57:28 +0200

    feat: auth errors

--- src/common/errorHandler.js
+++ src/common/errorHandler.js
@@ -1,11 +1,19 @@
-const notFoundError = require('./errors');
+const {
+  NotFoundError,
+  AuthorizationError,
+  UnauthorizedError
+} = require('./errors');
 
 const handleErrors = (err, req, res, next) => {
-  if (err instanceof notFoundError) {
+  if (
+    err instanceof NotFoundError ||
+    err instanceof AuthorizationError ||
+    err instanceof UnauthorizedError
+  ) {
     res.status(err.status).send(err.message);
   } else if (err) {
     console.error(err);
-    res.status(500).send({ message: 'Internal Server Error' });
+    res.status(500).send('Internal Server Error');
   }
   next();
 };
